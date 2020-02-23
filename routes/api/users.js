26a9e5be82f182d8f55cd0a3ff4f382b4cc61aa4
sharshi <sharshib@gmail.com>
2020-02-23 16:20:44 -0500

    Add session token

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+
 const router = express.Router();
 
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
+
+const User = require('../../models/User');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
@@ -22,10 +28,21 @@ router.post('/register', (req, res) => {
       return res.status(400).json(errors);
     } else {
       const newUser = new User({
-        name: req.body.name,
+        handle: req.body.handle,
         email: req.body.email,
         password: req.body.password
       })
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        })
+      })
+
     }
   })
 })
@@ -50,12 +67,23 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            res.json({ msg: 'Success' });
+            const payload = { id: user.id, handle: user.handle };
+
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              });
           } else {
-            errors.password = 'Incorrect password'
-            return res.status(400).json(errors);
+            return res.status(400).json({ password: 'Incorrect password' });
           }
         })
+
     })
 })
 
