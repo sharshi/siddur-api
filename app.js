df24ca5cd57fc9630f1c804bf88a1072ca374794
Shersheial Borisute <sharshib@gmail.com>
2020-06-03 14:19:29 -0400

    add path

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const passport = require('passport');
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/public"));
+  app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
