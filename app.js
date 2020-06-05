bf6fbdbe2e398814813a51abbb127c4a8f2ccaf3
Shersheial Borisute <sharshib@gmail.com>
2020-06-05 17:35:08 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+const passport = require('passport');
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+  app.use("/api/users", users);
+  app.use("/api/tweets", tweets);
