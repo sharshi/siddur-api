baf3cc849223d8e6e61662008a0de35edf55fbdf
Shersheial Borisute <4740525+sharshi@users.noreply.github.com>
2022-04-06 00:40:48 -0400

    Fix linting error

--- frontend/src/components/main/app_card.js
+++ frontend/src/components/main/app_card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image, Text, Group, Badge, createStyles, Center, Button } from '@mantine/core';
+import { Card, Image, Text, Group, Badge, createStyles, Button } from '@mantine/core';
 import { Link } from "react-router-dom";
 
 const useStyles = createStyles((theme) => ({
