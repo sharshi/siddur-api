29e4833d08131d849cc63602304f153afcc8986d
Shersheial Borisute <4740525+sharshi@users.noreply.github.com>
2022-04-12 23:13:59 -0400

    Add error actions

---
+++ frontend/src/actions/constants.actions.js
@@ -0,0 +1,1 @@
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
--- frontend/src/actions/siddur_actions.js
+++ frontend/src/actions/siddur_actions.js
@@ -1,17 +1,29 @@
 import * as SiddurUTIL from "../util/siddur_util";
 
+import { CLEAR_ERRORS } from "../actions/constants.actions";
 export const RECEIVE_SIDDUR = "RECEIVE_SIDDUR";
 export const UPDATE_SIDDUR_PARAGRAPH = "UPDATE_SIDDUR_PARAGRAPH";
 export const RECEIVE_SIDDUR_PARAGRAPHS = "RECEIVE_SIDDUR_PARAGRAPHS";
+export const RECEIVE_SIDDUR_ERRORS = "RECEIVE_SIDDUR_ERRORS";
+
 
 export const receiveSiddur = paragraph => ({
   type: RECEIVE_SIDDUR,
   paragraph
 });
 
+export const receiveSiddurErrors = errors => ({
+  type: RECEIVE_SIDDUR_ERRORS,
+  errors
+});
+
 export const receiveSiddurParagraphs = paragraphs => ({
   type: RECEIVE_SIDDUR_PARAGRAPHS,
   paragraphs
+});
+
+export const clearErrors = () => ({
+  type: CLEAR_ERRORS
 });
 
 export const fetchSiddur = () => dispatch => {
@@ -32,8 +44,8 @@ export const findParagraph = name => dispatch => {
 
 export const updateParagraph = paragraph => dispatch => {
   SiddurUTIL.updateParagraph(paragraph)
-    .then(paragraph => {
-      console.log(paragraph, 'updateParagraph');
-      return dispatch(receiveSiddur(paragraph.data))
-    });
+    .then(
+      paragraph => dispatch(receiveSiddur(paragraph.data)),
+      errors => dispatch(receiveSiddurErrors(errors.response.data))
+    );
 };
